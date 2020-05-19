29e9366bbc082ec5439c9bd51f772f2a269d6034
michaelhans <michaelhans777@gmail.com>
2020-05-19 10:27:19 +0700

    Update LoadVocabulary.py

--- src/LoadVocabulary.py
+++ src/LoadVocabulary.py
@@ -1,4 +1,5 @@
 # Load Vocabulary Module
+
 from PatternBM import *
 from PatternKMP import *
 from PatternRegex import *
@@ -16,61 +17,89 @@ def LoadVocabulary(fileName) :
         vocab.update({temp[0] : temp[1]})
     return vocab
 
+# Pattern Matching sorted by user choice
+def PatternMatching(text, pattern, choice):
+    if (choice == 1):
+        return BMPatternMatching(text, pattern)
+    elif (choice == 2):
+        return KMPPatternMatching(text, pattern)
+    elif (choice == 3):
+        return RegexPatternMatching(text, pattern)
+
 # Translate Sunda into Indonesia
 def SundaToIndoTranslator(buffer, SundaToIndo, choice):
     result = buffer.split(" ")
-    if (choice == 1):
-        for i in range(len(result)):
-            found = False
-            newWord = ""
-            for grammar in SundaToIndo:
-                if (BMPatternMatching(grammar, result[i])):
-                    found = True
-                    print(result[i], grammar)
-                    newWord = SundaToIndo[grammar]
-                    break
-            if (found):
-                result[i] = newWord
-            print(result)
+    for i in range(len(result)):
+        found = False
+        newWord = ""
+        for grammar in SundaToIndo:
+            if (PatternMatching(result[i], grammar, choice)):
+                found = True
+                print(result[i], grammar)
+                newWord = SundaToIndo[grammar]
+                break
+        if (found):
+            result[i] = newWord
+        print(result)
 
-        final = ""
-        if (len(result) != 0):
-            final = final + result[0]
-            for i in range(1, len(result)):
-                final = final + " " + result[i]
-                
-    # elif (choice == 2):
-    #     for grammar in SundaToIndo:
-    #         if (KMPPatternMatching(result, grammar)):
-    #             result = result.replace(grammar, SundaToIndo[grammar])
-    # elif (choice == 3):
-    #     for grammar in SundaToIndo:
-    #         if (RegexPatternMatching(result, grammar)):
-    #             result = result.replace(grammar, SundaToIndo[grammar])
+    final = ""
+    if (len(result) != 0):
+        final = final + result[0]
+        for i in range(1, len(result)):
+            final = final + " " + result[i]
     return final
 
-# def IndoToSundaTranslator(buffer, IndoToSunda, choice):
+# Translate Indonesia to Sunda
+def IndoToSundaTranslator(buffer, IndoToSunda, choice):
+    result = buffer.split(" ")
+    for i in range(len(result)):
+        found = False
+        newWord = ""
+        for grammar in IndoToSunda:
+            if (PatternMatching(result[i], grammar, choice)):
+                found = True
+                print(result[i], grammar)
+                newWord = IndoToSunda[grammar]
+                break
+        if (found):
+            result[i] = newWord
+        print(result)
 
+    final = ""
+    if (len(result) != 0):
+        final = final + result[0]
+        for i in range(1, len(result)):
+            final = final + " " + result[i]
+    return final
 
-# Translate Indonesia into Sunda
+# Load Vocabulary Indonesia-Sunda and Sunda-Indonesia
 IndoToSunda = {}
 SundaToIndo = {}
 IndoToSunda = LoadVocabulary("indonesia.txt")
 SundaToIndo = LoadVocabulary("sunda.txt")
 
+# Choose preference method for pattern matching
+print("Simple Sundanese Translator")
+print("Created by: 13518056 - Michael Hans")
 print("Terdapat 3 buah metode pencocokan string:")
 print("1. Algoritma Boyer-Moore")
 print("2. Algoritma KMP")
 print("3. Regular Expression")
 choice = int(input("Masukkan pilihan metode pencocokan string: "))
 
+# Choose Translation Method
 print("Terdapat 2 metode penerjemahan:")
-print("1. Indonesia-Sunda")
-print("2. Sunda-Indonesia")
+print("1. Sunda-Indonesia")
+print("2. Indonesia-Sunda")
 method = int(input("Masukkan pilihan metode: "))
+
+# Input text that want to be translated
 buffer = input("Input text: ")
-result = SundaToIndoTranslator(buffer, SundaToIndo, choice)
-print(SundaToIndo.get("ti"))
-print("Hasil :",result)
-
-
+if (method == 1):
+    result = SundaToIndoTranslator(buffer, SundaToIndo, choice)
+    print("Sunda     :", buffer)
+    print("Indonesia :", result)
+else:
+    result = IndoToSundaTranslator(buffer, IndoToSunda, choice)
+    print("Indonesia :", buffer)
+    print("Sunda     :", result)
